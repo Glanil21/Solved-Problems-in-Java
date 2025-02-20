7ab33008fddd84bd2469653ec31d087f697bdebc
Glanil21 <glaniltauro.aiet@gmail.com>
2025-02-21 00:14:27 +0530

    Added remaining Java programs

--- prg23.java
+++ prg23.java
@@ -1,24 +1,29 @@
 public class prg23 {
+
         public static void main(String[] args) {
-            int n = 1124;
+            int n = 124;
             spy(n);
         }
     
-        public static void spy(int n) {
-            int sum = 0, product = 1, temp = n;
+        public static void spy(int n){
     
-            while (temp > 0) {
-                int digit = temp % 10;
-                sum += digit;
-                product *= digit;
-                temp /= 10;
+    
+            int sum = 0; int product = 1;
+            while(n > 0){
+                int rem = n % 10;
+                sum = sum + rem;
+                product = product * rem;
+                n = n / 10;
+            }
+            if(sum == product){
+                System.out.println("The number is a Spy number");
+            }
+            else {
+                System.out.println("Not a spy number");
             }
     
-            if (sum == product) {
-                System.out.println("Spy Number");
-            } else {
-                System.out.println("Not a Spy Number");
-            }
         }
     }
+    
+    
     
